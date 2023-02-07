8befeba459902c15c2aea4666b3ec1412b86000f
skiotto93 <fabio.forenza@hotmail.com>
2023-02-07 23:26:09 +0100

    aggiunta funzione project.php per img front-end

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $guarded = ['slug'];
+    protected $appends = ['image_url'];
+
+    protected function getImageUrlAttribute() {
+        return $this->cover_image ? asset("storage/$this->cover_image") : "https://placeholder.com/assets/images/150x150-2-500x500.png";
+    }
 
     public function type() {
         return $this->belongsTo(Type::class);
